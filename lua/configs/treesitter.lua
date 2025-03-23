e346e08d55b1fb8b88d9bb1c0402e44c9ef9d081
Your Name <140066704+Parth002933v@users.noreply.github.com>
2025-03-23 23:19:40 +0530

    fix(treesitter) : add only necessary parsers

--- lua/configs/treesitter.lua
+++ lua/configs/treesitter.lua
@@ -1,5 +1,42 @@
 local opts = {
-  ensure_installed = "all",
+  ensure_installed = {
+    -- Web development
+    "html",
+    "css",
+    "javascript",
+    "typescript",
+    "tsx",
+    "php",
+    
+    -- Config files
+    "json",
+    "yaml",
+    "toml",
+    "dockerfile",
+    
+    -- Programming languages
+    "lua",
+    "python",
+    "c",
+    "cpp",
+    "go",
+    
+    -- Markup
+    "markdown",
+    "markdown_inline",
+    
+    -- Git
+    "git_config",
+    "git_rebase",
+    "gitcommit",
+    "gitignore",
+    
+    -- Others
+    "vim",
+    "query",
+    "regex",
+    "bash"
+  },
   indent = {
     enable = true,
   },
